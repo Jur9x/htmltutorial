b0c97cbc6084e9a7b43f5eeeb5753b4890e01621
Jur9x <jirka.ryba02@gmail.com>
2021-04-11 21:47:11 +0200

    2.1 Upraven disco mod

--- disco.js
+++ disco.js
@@ -2,17 +2,25 @@ var number = 0;
 
 function disco() {
     if (number == 0){
-    document.getElementById('button').classList.add('disco');
-    document.getElementById('main').classList.add('disco');
-    document.getElementById('button').classList.remove('discono');
-    document.getElementById('main').classList.remove('discono');
+    document.getElementById('button').classList.add('discobck');
+    document.getElementById('main').classList.add('discobck');
+    document.getElementById('disco1').classList.add('discoclr');
+    document.getElementById('disco2').classList.add('discoclr');
+    document.getElementById('disco3').classList.add('discoclr');
+    document.getElementById('disco4').classList.add('discoclr');
+    document.getElementById('disco5').classList.add('discoclr');
+    document.getElementById('disco6').classList.add('discoclr');
     return number = 1;
     }
     if (number == 1){
-        document.getElementById('button').classList.add('discono');
-        document.getElementById('button').classList.remove('disco');
-        document.getElementById('main').classList.add('discono');
-        document.getElementById('main').classList.remove('disco');
+        document.getElementById('button').classList.remove('discobck');
+        document.getElementById('main').classList.remove('discobck');
+        document.getElementById('disco1').classList.remove('discoclr');
+        document.getElementById('disco2').classList.remove('discoclr');
+        document.getElementById('disco3').classList.remove('discoclr');
+        document.getElementById('disco4').classList.remove('discoclr');
+        document.getElementById('disco5').classList.remove('discoclr');
+        document.getElementById('disco6').classList.remove('discoclr');
         return number = 0;
     }   
 }
